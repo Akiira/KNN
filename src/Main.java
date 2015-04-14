33ff9f41b456ddab9492876e76cb2cc5b9d919b9
ikepolinsky <ikepolinsky72@gmail.com>
2015-04-14 17:21:31 -0400

    new

---
+++ src/Main.java
@@ -0,0 +1,230 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+enum DistanceFunction { BAG, SET }
+enum VoteFunction { EQUAL, DISTANCESQ, SCALEDDIST }
+
+// SET, DISTANCESQ = 89.7% (k = 10, o = 0, s = 100)
+// BAG, DISTANCESQ = 88.8% (k = 10, o = 0, s = 100)
+// SET, EQUAL = 88.?% (k = 10, o = 0, s = 100)
+// BAG, EQUAL = 88.5% (k = 10, o = 0, s = 100)
+// SET, SCALEDDIST = 89.3% (k = 10, o = 0, s = 100)
+// BAG, SCALEDDIST = 89.3% (k = 10, o = 0, s = 100)
+// Using leave one out
+
+public class Main {
+
+	public static HashSet<String> tabooList;
+	static ArrayList<Recipe> trainingData;
+	public static int k = 10;
+	public static int o = 0; // o <= 1 will not add a word to taboo list
+	public static int s = 100; // s >= 100 will not add a word to taboo list
+	public static double minDist;
+	public static String trainingFile = "training-data.txt";
+	public static PriorityQueue<Recipe> neighbors;
+	public static DistanceFunction df = DistanceFunction.BAG;
+	public static VoteFunction vf = VoteFunction.SCALEDDIST; 
+	
+	public static void main(String[] args) {
+		tabooList = new HashSet<String>();
+		buildTabooList();
+		readTrainingSet();
+		crossValidate();
+		//runOnTestData();
+	}
+	
+	public static void buildTabooList() {
+		try {
+			tabooList = new HashSet<String>();
+			String line;
+			String splitLine[];
+			String splitProportions[];
+			BufferedReader br = new BufferedReader(new FileReader(new File("all.txt")));
+			while ((line = br.readLine()) != null) {
+				splitLine = line.split(":");
+				if (splitLine.length != 2) {
+					System.out.println("ERROR? " + line);
+				}
+				splitProportions = splitLine[1].split(" ");
+				double min, max;
+				min = max = Double.parseDouble(splitProportions[0]);
+				for (int i = 1; i < splitProportions.length; i++) {
+					double current = Double.parseDouble(splitProportions[i]);
+					min = min < current ? min : current;
+					max = max > current ? max : current;
+				}
+				if (max - min > s) {
+					tabooList.add(splitLine[0]);
+				}
+			}
+			br.close();
+			
+			br = new BufferedReader(new FileReader(new File("one.txt")));
+			while ((line = br.readLine()) != null) {
+				splitLine = line.split(":");
+				if (splitLine.length != 2) {
+					System.out.println("ERROR? " + line);
+				}
+				if (Integer.parseInt(splitLine[1]) < o ) {
+					tabooList.add(splitLine[0]);
+				}
+			}
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void readTrainingSet() {
+		trainingData = new ArrayList<Recipe>();
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(new File(trainingFile)));
+			String line;
+			int recipeNum = 0;
+			while ((line = br.readLine()) != null) {
+				if (line.length() == 2) { 
+					System.err.println("Found bad data");
+					continue;
+				}
+				trainingData.add(new Recipe(true, line, recipeNum));
+				recipeNum++;
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static int calculateVotes() {
+		int cuisine = 1;
+		double[] votes = new double[8];
+		double maxVotes;
+		double maxDist;
+		
+		switch (vf){
+			case EQUAL:
+				while (neighbors.peek() != null) {
+					votes[neighbors.poll().cuisine]++;
+				}
+				
+				maxVotes = votes[1];
+				for (int i = 2; i < 8; i++) {
+					if (votes[i] > maxVotes) {
+						maxVotes = votes[i];
+						cuisine = i;
+					}
+				}
+				break;
+			case DISTANCESQ:
+				while (neighbors.peek() != null) {
+					Recipe current = neighbors.poll();
+					votes[current.cuisine] += (1/Math.pow(current.distance,2));
+				}
+				
+				maxVotes = votes[1];
+				for (int i = 2; i < 8; i++) {
+					if (votes[i] > maxVotes) {
+						maxVotes = votes[i];
+						cuisine = i;
+					}
+				}
+				break;
+			case SCALEDDIST:
+				maxDist = neighbors.peek().distance;
+				
+				while (neighbors.peek() != null) {
+					Recipe current = neighbors.poll();
+					votes[current.cuisine] += (maxDist - current.distance)/(maxDist - minDist);
+				}
+				
+				maxVotes = votes[1];
+				for (int i = 2; i < 8; i++) {
+					if (votes[i] > maxVotes) {
+						maxVotes = votes[i];
+						cuisine = i;
+					}
+				}
+				break;
+		}
+		
+		return cuisine;
+	}
+	
+	public static void crossValidate() {
+		int total = 0;
+		int correct = 0;
+		for (int i = 0 ; i < trainingData.size(); i++) {
+			neighbors = new PriorityQueue<Recipe>(k);
+			minDist = 1;
+			for (int j = 0; j < trainingData.size(); j++) {
+				if (i == j) {
+					continue;
+				}
+				
+				double distance = trainingData.get(j).getDistance(trainingData.get(i));
+				
+				minDist = minDist < distance ? minDist : distance;
+				
+				if (neighbors.size() < k) {
+					neighbors.add(trainingData.get(j));
+				} else {
+					if (neighbors.peek().distance > distance) {
+						neighbors.poll();
+						neighbors.add(trainingData.get(j));
+					}
+				}	
+			}
+			
+			// vote
+			int prediction = calculateVotes();
+			
+			// check accuracy
+			if (prediction == (trainingData.get(i).cuisine)) {
+				correct++;
+			}
+			total++;
+			
+			if (total % 500 == 0) {
+				System.err.println(total + " " + (double)correct / total);
+			}
+		}
+		
+		System.out.println((double)correct / total);
+	}
+	
+	public static void runOnTestData() {
+		Scanner sc = new Scanner(System.in);
+		String line;
+		while ((line = sc.nextLine()) != null) {
+			Recipe current = new Recipe(false, line, 0);
+			neighbors = new PriorityQueue<Recipe>(k);
+			minDist = 1;
+			for (int j = 0; j < trainingData.size(); j++) {
+				
+				double distance = trainingData.get(j).getDistance(current);
+				
+				minDist = minDist < distance ? minDist : distance;
+				
+				if (neighbors.size() < k) {
+					neighbors.add(trainingData.get(j));
+				} else {
+					if (neighbors.peek().distance > distance) {
+						neighbors.poll();
+						neighbors.add(trainingData.get(j));
+					}
+				}	
+			}
+			
+			// vote
+			System.out.println(calculateVotes());
+		}
+		sc.close();
+	}
+}
---
+++ src/Recipe.java
@@ -0,0 +1,148 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+
+
+public class Recipe implements Comparable, Comparator {
+	public int cuisine;
+	public HashSet<String> ingredientSet;
+	public HashMap<String, Integer> ingredientBag;
+	public int recipeNum;
+	public double distance;
+	
+	public Recipe(boolean training, String line, int recipeNum) {
+		this.recipeNum = recipeNum;
+		ingredientSet = new HashSet<String>();
+		ingredientBag = new HashMap<String, Integer>();
+		String[] lineArray = line.split(" ");
+		int startIndex;
+		if (training) {
+			cuisine = Integer.parseInt(lineArray[0]);
+			startIndex = 1;
+		}
+		else {
+			cuisine = -1; 
+			startIndex = 0;
+		}
+		
+		// build ingredient set
+		for (int i = startIndex; i < lineArray.length; i++) {
+			if (!(Main.tabooList.contains(lineArray[i]))) {
+				ingredientSet.add(lineArray[i]);
+			}
+		}
+					
+		// build ingredient bag
+		for (int i = startIndex; i < lineArray.length; i++) {
+			if (!(Main.tabooList.contains(lineArray[i]))) {
+				if (!(ingredientBag.containsKey(lineArray[i]))) {
+					ingredientBag.put(lineArray[i], 0);
+				}
+						
+				ingredientBag.put(lineArray[i], ingredientBag.get(lineArray[i]) + 1);
+			}
+		}
+			
+		if (ingredientSet.size() == 0) {
+			System.out.println("Empty recipe: " + line + " (" + line.length() + ")");
+		}
+	}
+	
+	public double getDistance(Recipe other) {
+		double unionSize = 0;
+		double intersectSize = 0;
+		switch (Main.df) {
+			case BAG:
+				for (String word : this.ingredientBag.keySet()) {
+					if (other.ingredientBag.containsKey(word)) {
+						intersectSize += Math.min(this.ingredientBag.get(word), other.ingredientBag.get(word));
+					}
+				}
+				
+				for (String word : this.ingredientBag.keySet()) {
+					unionSize += this.ingredientBag.get(word);
+				}
+				
+				for (String word : other.ingredientBag.keySet()) {
+					unionSize += other.ingredientBag.get(word);
+				}
+				
+				distance = 1 - intersectSize / unionSize;
+				break;
+			case SET:
+				HashSet<String> union = new HashSet<String>();
+				union.addAll(this.ingredientSet);
+				union.addAll(other.ingredientSet);
+				unionSize = union.size();
+				
+				for (String ingr : this.ingredientSet) {
+					if (other.ingredientSet.contains(ingr)) {
+						intersectSize++;
+					}
+				}
+				
+				distance =  1 - intersectSize / unionSize;
+				break;
+		}
+		
+		return distance;
+	}
+	
+	public boolean equals(Object obj) {
+		if(this == obj) return true;
+		if((obj == null) || (obj.getClass() != this.getClass())) return false;
+		Recipe other = (Recipe)obj;
+		boolean equal = true;
+		equal &= cuisine != -1 && other.cuisine != -1; // They are both from the training set
+		if (equal) equal &= cuisine == other.cuisine;	
+		if (equal) equal &= ingredientSet.size() == other.ingredientSet.size();
+		if (equal) ingredientSet.equals(other.ingredientSet);
+		
+		return equal;
+	}
+	
+
+	public int hashCode() {
+		int hash = 7;
+		hash = 31 * hash + cuisine;
+		hash = 31 * hash + ingredientSet.hashCode();
+		hash = 31 * hash + recipeNum;
+		return hash;
+	}
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(cuisine + " ");
+		for (String s : ingredientSet) {
+			sb.append(s + " ");
+		}
+		return sb.toString();
+	}
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		Recipe r1 = (Recipe)o1;
+		Recipe r2 = (Recipe)o2;
+		
+		if (r1.distance > r2.distance) {
+			return -1;
+		} else if (r1.distance < r2.distance) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		Recipe other = (Recipe)o;
+		
+		if (this.distance > other.distance) {
+			return -1;
+		} else if (this.distance < other.distance) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+}
